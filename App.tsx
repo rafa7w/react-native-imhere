f15975a60ae9b1e4529c526aa1e42fd4bf19ffe6
Rafael William <rafael.fim@hotmail.com>
2023-05-05 16:08:23 -0300

    statusbar

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
 // Por padrão abre os arquivos chamados index
+import { StatusBar } from 'react-native'
 import { Home } from './src/screens/Home'
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+
+      />
+      <Home />
+    </>
   )
 }
